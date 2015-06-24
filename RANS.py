687a067d01f5d60bce5349ff5ac79de595ec22fa
Mike Park <Mike.Park@NASA.gov>
2015-06-24 15:35:38 -0500

    p3d and 4 grids with physics in filename

--- RANS.py
+++ RANS.py
@@ -4,5 +4,5 @@ Q = 4
 for ref in xrange(0,1):
     make_airfoil(100, ref, Q, False, "grm", nchordwise=8, nxwake=8, nnormal=14,
                  rnormal=4, rnormalfar=4, rxwakecenter=3.65, reynolds=1.e6,
-                 filename_base="Joukowski")
+                 filename_base="Joukowski_RANS")
     print("Done with level " + str(ref));
